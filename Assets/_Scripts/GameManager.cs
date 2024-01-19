0104baab2ee35ef6c21ec499b7b7b982ee180e04
AlmikSS <ulmasoviskandar7@gmail.com>
2024-01-19 20:30:48 +0300

    Добавил таймер

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,17 +5,36 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
     [SerializeField] private TMP_Text _timerText;
-
+    [SerializeField] private float _levelTime;
+    [SerializeField] private int _elementCountForWin;
+    
     private static GameManager _instance;
-    private int score;
+    private Timer _timer;
+    private int _score;
 
     private void Start()
     {
         _instance = this;
+        _timer = new Timer(this);
+        _timer.Set(_levelTime);
+        _timer.HasBeenUpdate += UpdateTimer;
+        _timer.TimeIsOver += Lose;
+        _timer.StartCountingTime();
     }
 
-    public static void AddSore()
+    private void Lose()
     {
-        
+        Debug.Log("Los");
+    }
+    
+    private void UpdateTimer(float remineTime)
+    {
+        _timerText.text = $"Time: {Mathf.RoundToInt(remineTime)}";
+    }
+    
+    private void AddSore()
+    {
+        _instance._score++;
+        _instance._scoreText.text = $"Score: {_instance._score}";
     }
 }
---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Timer
+{
+    private float _time;
+    private float _reminingTime;
+    
+    public event Action TimeIsOver;
+    public event Action<float> HasBeenUpdate;
+
+    private MonoBehaviour _context;
+    private IEnumerator _countDown;
+    
+    public Timer(MonoBehaviour context) => _context = context;
+
+    public void Set(float time)
+    {
+        _time = time;
+        _reminingTime = _time;
+    }
+
+    public void StartCountingTime()
+    {
+        _countDown = CountDown();
+        _context.StartCoroutine(_countDown);
+    }
+
+    public void StopCountingTime()
+    {
+        if (_countDown != null)
+            _context.StopCoroutine(_countDown);
+    }
+
+    private IEnumerator CountDown()
+    {
+        while (_reminingTime >= 0)
+        {
+            _reminingTime -= Time.deltaTime;
+            
+            HasBeenUpdate?.Invoke(_reminingTime);
+            
+            yield return null;
+        }
+        
+        TimeIsOver?.Invoke();
+    }
+}
